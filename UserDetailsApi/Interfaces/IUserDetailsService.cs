7f7a44eeaeb5a0b7b6a5d49451e902b897dc6480
Thero-Malepe <163337128+Thero-Malepe@users.noreply.github.com>
2026-03-06 21:41:16 +0200

    implemented authentication and details service

---
+++ UserDetailsApi/Interfaces/IUserDetailsService.cs
@@ -0,0 +1,11 @@
+﻿using UserDetailsApi.DTOs;
+using UserDetailsApi.Models;
+
+namespace UserDetailsApi.Interfaces
+{
+    public interface IUserDetailsService
+    {
+        Task<UserDetailsDto?> GetUserDetails(string userDto);
+        Task<TokenResponseDto?> UpdateUserDetails(LoginDto apiUserloginDto);
+    }
+}
---
+++ UserDetailsApi/Services/UserDetailsService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserDetailsApi.Data;
+using UserDetailsApi.DTOs;
+using UserDetailsApi.Interfaces;
+using UserDetailsApi.Migrations;
+
+namespace UserDetailsApi.Services
+{
+    public class UserDetailsService(UserDetailsDbContext context) : IUserDetailsService
+    {
+        public async Task<UserDetailsDto?> GetUserDetails(string userEmail)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(u => u.Email.ToLower() == userEmail.ToLower());
+
+            if (user is null)
+            {
+                return null;
+            }
+            var userDetailsDto = new UserDetailsDto
+            {
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+            };
+            
+            return userDetailsDto;
+        }
+
+        public Task<TokenResponseDto?> UpdateUserDetails(LoginDto apiUserloginDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
